71db770f2964d73e7987c96e4999a5ef32115bc7
Jacob Egner <jmegner@gmail.com>
2015-06-21 03:35:22 -0500

    further in djikstra algo

--- maze.py
+++ maze.py
@@ -3,9 +3,9 @@
 
 from __future__ import division
 from __future__ import print_function
+import collections
+import copy
 import sys
-# import argparse
-# import fileinput
 
 
 class Maze:
@@ -21,23 +21,12 @@ class Maze:
     c_path = '.'
     c_start = 'S'
     c_finish = 'F'
-    c_validChars = {c_wall, c_open, c_path, c_start, c_finish}
+    c_validInputChars = {c_wall, c_open, c_start, c_finish}
 
 
-    class Coord:
-        def __init__(self, r, c, dist = c_maxDist):
-            self.r = r
-            self.c = c
-            self.dist = dist
+    RcTuple = collections.namedtuple('RcTuple', ['r', 'c'])
 
-
-        def __cmp__(self, other):
-            return cmp(self.asTuple(), other.asTuple())
-
-
-        def asTuple(self):
-            return (self.dist, self.r, self.c)
-
+    class Coord(RcTuple):
 
         def up(self):
             return Coord(self.r - 1, self.c)
@@ -59,35 +48,43 @@ class Maze:
             return [ self.up(), self.right(), self.down(), self.left(), ]
 
 
-    def __init__(self, rows):
-        self.grid = rows
-        self.numRows = len(rows)
-        self.numCols = len(rows[0])
-        self.startCoord = self.coordOfCellType(c_start)
-        self.finishCoord = self.coordOfCellType(c_finish)
-        self.dists = [ [None] * self.numCols for i in range(N) ]
+    def __init__(self, cells):
+        'cells is 2d list of chars'
+
+        self.cells = cells
+
+        self.numRows = len(cells)
+        self.numCols = len(cells[0])
+
+        #self.startCoord = self.coordOfCellType(self.c_start)
+        #self.finishCoord = self.coordOfCellType(self.c_finish)
+
+        self.dists = [ [self.c_maxDist] * self.numCols
+            for i in range(self.numRows) ]
+        self.pathedCells = copy.deepcopy(self.cells)
+        self.pathCoords = []
 
 
     @staticmethod
     def fromFile(inputFile):
-        grid = []
+        cells = []
 
         for textLine in inputFile:
             if len(textLine) == 0:
                 continue
 
             row = list(textLine.rstrip('\r\n'))
-            _addRow(grid, row)
+            Maze._addRow(cells, row)
 
-        return Maze(grid)
+        return Maze(cells)
 
 
     def __str__(self):
-        return '\n'.join([''.join(row) for row in self.grid])
+        return '\n'.join([''.join(row) for row in self.pathedCells])
 
 
     def getCell(self, coord):
-        return self.grid[coord.r][coord.c]
+        return self.cells[coord.r][coord.c]
 
 
     def getDist(self, coord):
@@ -99,7 +96,7 @@ class Maze:
 
 
     def getWalkableNeighbors(self, coord):
-        [Coord(neighbor.r, neighbor.c, self.getDist(neighbor))
+        [self.Coord(neighbor.r, neighbor.c, self.getDist(neighbor))
                 for neighbor in coord.naiveNeighbors()
                 if self.inBounds(neighbor) and self.getCell(neighbor) == c_open]
 
@@ -109,39 +106,54 @@ class Maze:
             and coord.r < self.numRows and coord.c < self.numCols)
 
 
+    def getMinDistCoord(self, coords):
+        min(coords,
+            key = lambda a, b : a if self.getDist(a) < self.getDist(b) else b)
+
+
     def solve(self):
-        solvedCoords = {}
-        unsolvedCoords = []
+        solvedCoords = set()
+        unsolvedCoords = set()
 
         for r in range(self.numRows):
             for c in range(self.numCols):
-                dist = c_maxDist
-                if self.grid[r][c] == c_start:
-                    dist = 0
-                heapq.heappush(unsolvedCoords, Coord(r, c, dist))
+                if self.cells[r][c] == self.c_start:
+                    self.dists[r][c] = 0
+                else:
+                    self.dists[r][c] = self.c_maxDist
+
+                if self.cells[r][c] != self.c_wall:
+                    unsolvedCoords.add(self.Coord(r, c))
 
         foundFinish = False
 
         while not foundFinish and unsolvedCoords:
-            coord = heapq.heappop(unsolvedCoords)
-            solvedCoords.add(coord)
-            neighbors = self.getWalkableNeighbors(coord)
+            newlySolvedCoord = self.getMinDistCoord(unsolvedCoords)
+            newFrontierDist = self.getDist(newlySolvedCoord) + 1
+
+            unsolvedCoords.remove(newlySolvedCoord)
+            solvedCoords.add(newlySolvedCoord)
+
+            neighbors = self.getWalkableNeighbors(newlySolvedCoord)
 
             for neighbor in neighbors:
-                raise NotImplementedError("do comparison of neighbor.dist to new dist")
-                self.setDist(neighbor, currentDist)
+                if newFrontierDist < neighbor.dist:
+                    self.setDist(neighbor, newFrontierDist)
 
+                    if self.getCell(neighbor) == self.c_finish:
+                        foundFinish = True
+                        break
 
 
     @staticmethod
-    def _addRow(grid, row):
-        invalidChars = set(row) - Maze.c_validChars
+    def _addRow(cells, row):
+        invalidChars = set(row) - Maze.c_validInputChars
         if len(invalidChars) > 0:
             raise ValueError(
                 "invalid maze characters: {}".format(invalidChars))
 
-        if len(grid) == 0 or len(row) == len(grid[0]):
-            grid.append(row)
+        if len(cells) == 0 or len(row) == len(cells[0]):
+            cells.append(row)
         else:
             raise ValueError("maze rows should all be same length")
 
@@ -151,12 +163,17 @@ def main(argv = None):
         print("usage: maze.py MAZE_FILE [MAZE_FILE ...]")
         return -1
 
+    if True:
+        fiddleVar = 7
+    print("fiddleVar={}".format(fiddleVar))
+
     for inputFileName in argv[1:]:
         print("processing {}".format(inputFileName))
         inputFile = open(inputFileName)
         maz = Maze.fromFile(inputFile)
 
         print("{}\n{}".format(inputFileName, maz))
+        maz.solve()
 
     return 0
 
